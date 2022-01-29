aa96f0b3599dc4bb82c86cff355f7e1ae477db1d
김 유림 <soo054141@gmail.com>
2022-01-29 11:19:32 +0000

    220129 1주차 - 프론트엔드 MR 요청

---
+++ Frontend/src/styles/GlobalStyles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 10px;
+        vertical-align: baseline;
+    }
+    body{
+        line-height: 1;
+        font-family: 'Noto Sans KR', sans-serif;
+        
+    }
+    ol, ul{
+        list-style: none;
+    }
+`;
+
+export default GlobalStyles;
