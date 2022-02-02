e6827903b7df4a08472ea2a36aab5889e1a4363e
지영 서 <stseo012@gmail.com>
2022-02-02 20:55:12 +0000

    220203 2주차 - 프론트엔드 MR 요청

--- Frontend/src/styles/GlobalStyles.js
+++ Frontend/src/styles/GlobalStyles.js
@@ -21,10 +21,15 @@ const GlobalStyles = createGlobalStyle`
     body{
         line-height: 1;
         font-family: 'Noto Sans KR', sans-serif;
-        
+        background-color: #F6F9F0;
     }
     ol, ul{
         list-style: none;
+    }
+    button {
+        border: 0;
+        background: transparent;
+        cursor: pointer;
     }
 `;
 
---
+++ Frontend/src/styles/theme.js
@@ -0,0 +1,15 @@
+const size = {
+  tablet: "640px",
+  laptop: "1200px",
+  desktop: "1800px",
+};
+
+const theme = {
+  mainColor: "#6ABD8C",
+  mainColorLight: "CCDFB0",
+  backgroundColor: "#F6F9F0",
+  darkGray: "#595959",
+  lightGray: "#939292",
+};
+
+export default theme;
