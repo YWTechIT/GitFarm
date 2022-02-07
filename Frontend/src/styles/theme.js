7838f61fd2097d6ecb81c0245d7aee28b7d87a27
sjy <stseo012@gmail.com>
2022-02-07 22:33:45 +0900

    Add theme color

--- Frontend/src/styles/theme.js
+++ Frontend/src/styles/theme.js
@@ -6,10 +6,11 @@ const size = {
 
 const theme = {
   mainColor: "#6ABD8C",
-  mainColorLight: "CCDFB0",
+  mainColorLight: "#CCDFB0",
   backgroundColor: "#F6F9F0",
   darkGray: "#595959",
   lightGray: "#939292",
+  superLightGray: "#F1F2F5",
 };
 
 export default theme;
