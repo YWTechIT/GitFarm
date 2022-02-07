a19e76e194f987a0fb7da1db160acbba3b288df8
sjy <stseo012@gmail.com>
2022-02-07 23:00:22 +0900

    Add Error Modal

---
+++ Frontend/src/components/ModalTypes/Error/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  width: 100%;
+  margin-bottom: 10px;
+  & > :nth-child(1) {
+    margin-top: 15px;
+  }
+  svg {
+    margin-left: 20px;
+    margin-top: -20px;
+    width: 80px;
+  }
+`;
