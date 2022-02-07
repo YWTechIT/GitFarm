f91ab34d1aa5066d2346254c2ca5a590289774f0
sjy <stseo012@gmail.com>
2022-02-07 22:50:56 +0900

    Add Btn component

---
+++ Frontend/src/components/Btn/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.div`
+  height: 35px;
+  line-height: 35px;
+  background-color: ${(props) =>
+    props.color === "gray"
+      ? props.theme.superLightGray
+      : props.theme.mainColorLight};
+  color: ${(props) =>
+    props.color === "gray" ? props.theme.lightGray : "white"};
+  width: ${(props) => (props.long ? "87.14%" : "40.51%")};
+  border-radius: 10px;
+  text-align: center;
+  font-size: 14px;
+  cursor: pointer;
+`;
