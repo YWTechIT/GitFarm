2644c63e10b1ee05145e7f5653308578d5d55ac7
지영 서 <stseo012@gmail.com>
2022-02-10 03:56:50 +0000

    220210 3주차 - 프론트엔드 MR 요청

--- Frontend/src/components/DateController/index.js
+++ Frontend/src/components/DateController/index.js
@@ -3,16 +3,24 @@ import * as Controller from "./style";
 import LeftArrow from "../../assets/icon/left-arrow.svg";
 import RightArrow from "../../assets/icon/right-arrow.svg";
 
-export function DateController({ date, clickLeft, clickRight, goToday }) {
+export function DateController({
+  date,
+  clickLeft,
+  clickRight,
+  goToday,
+  month,
+}) {
   return (
     <Controller.Container>
       <Controller.ArrowWrapper onClick={clickLeft}>
         <LeftArrow />
       </Controller.ArrowWrapper>
-      <Controller.DateText onClick={goToday}>
-        {date.getFullYear()}년
-        {date.getMonth() + 1 && `${date.getMonth() + 1}월`}
-      </Controller.DateText>
+      <Controller.TextWrapper>
+        <Controller.DateText onClick={goToday}>
+          {date.getFullYear()}년 {month && `${date.getMonth() + 1}월`}{" "}
+        </Controller.DateText>
+      </Controller.TextWrapper>
+
       <Controller.ArrowWrapper onClick={clickRight}>
         <RightArrow />
       </Controller.ArrowWrapper>
---
+++ Frontend/src/pages/graph/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const DateControllerWrapper = styled.div`
+  height: 20px;
+`;
