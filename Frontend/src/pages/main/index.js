c6a9b91a83ea1ee7d200c5a91d4cf8b257e8a1d9
지영 서 <stseo012@gmail.com>
2022-02-10 12:41:35 +0000

    220210 1주차 - 프론트엔드 MR 요청

--- Frontend/src/pages/main/index.js
+++ Frontend/src/pages/main/index.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { FarmPicture } from "./FarmPicture/";
 import { SpeechBubble } from "./SpeechBubble";
+import { CommitDetails } from "./CommitDetails";
 import * as Mains from "./style";
 
 Main.defaultProps = {
-  todaysCommit: 5,
+  todaysCommit: 6,
   goalCommit: 6,
 };
 export function Main({ todaysCommit, goalCommit }) {
   return (
     <Mains.Container>
       <Mains.TodaysCommit>오늘의 커밋</Mains.TodaysCommit>
-      <Mains.TodaysCommitCount>{todaysCommit}</Mains.TodaysCommitCount>
+      {goalCommit - todaysCommit !== 0 ? (
+        <Mains.TodaysCommitCount>
+          농장 완성까지 {goalCommit - todaysCommit}커밋 남았습니다!
+        </Mains.TodaysCommitCount>
+      ) : (
+        <Mains.TodaysCommitCount>
+          농장이 완성되었습니다. 축하합니다!
+        </Mains.TodaysCommitCount>
+      )}
       <FarmPicture ratio={Math.floor((todaysCommit / goalCommit) * 100)} />
-      <SpeechBubble leftCommit={goalCommit - todaysCommit} />
+      <CommitDetails />
+      {/* <SpeechBubble leftCommit={goalCommit - todaysCommit} /> */}
     </Mains.Container>
   );
 }
