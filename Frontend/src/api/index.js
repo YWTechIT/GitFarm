c97453c3411485a53cc9fe92c56b8f77a86d8036
sjy <stseo012@gmail.com>
2022-02-12 15:10:20 +0900

    Add url value

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const url = "http://localhost:8888";
+
 export const getBreeds = async () => {
   try {
-    const r = await axios.get("http://localhost:8888/posts");
+    const r = await axios.get(`${url}/posts`);
     console.log(r);
   } catch (error) {
     console.error(error);
