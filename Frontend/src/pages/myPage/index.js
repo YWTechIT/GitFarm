2672ac1184587959253f55ba85dac834921addc3
김 유림 <soo054141@gmail.com>
2022-02-12 16:37:08 +0000

    220212 3주차 - 프론트엔드 MR 요청 - Add mock for API 연결

--- Frontend/src/pages/myPage/index.js
+++ Frontend/src/pages/myPage/index.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@/components/Container/style";
+import * as api from "@/api";
 import StartedDayCount from "./StartedDayCount";
 import LevelInfo from "./LevelInfo";
 import CommitCount from "./CommitCount";
 import AccountSettings from "./AccountSettings";
 
 function MyPage() {
+  const [memberDate, setMemberDate] = useState(0);
+  const [score, setScore] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [continuous, setContinues] = useState(0);
+
+  const dataFunc = async () => {
+    const data = await api.getMyInfo();
+
+    if (data.success) {
+      const user = await data.mypage;
+      setMemberDate(user.memberDate);
+      setScore(user.score);
+      setTotal(user.total);
+      setContinues(user.continuous);
+    }
+  };
+
+  useEffect(() => {
+    dataFunc();
+  }, []);
+
   return (
     <Container>
-      <StartedDayCount />
-      <LevelInfo />
-      <CommitCount />
+      <StartedDayCount memberDate={memberDate} />
+      <LevelInfo score={score} />
+      <CommitCount total={total} continuous={continuous} />
       <AccountSettings />
     </Container>
   );
