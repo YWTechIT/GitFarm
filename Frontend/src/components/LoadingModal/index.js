11842a6d096a2e5d561ab822eb2aad812611f9fe
황원준 <dittoavec@gmail.com>
2022-02-13 10:30:31 +0000

    220213 3주차 - 프론트엔드 MR 요청

---
+++ Frontend/src/components/LoadingModal/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Wrapper, Loader } from "./style";
+
+function LoadingModal() {
+  return (
+    <Wrapper>
+      <Loader />
+    </Wrapper>
+  );
+}
+
+export default LoadingModal;
--- Frontend/src/pages/graph/index.js
+++ Frontend/src/pages/graph/index.js
@@ -1,16 +1,45 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState, Suspense } from "react";
 import { Container } from "@/components/Container/style";
 import DateController from "@/components/DateController";
-import LineGraph from "./LineGraph";
+import * as api from "@/api";
+import LoadingModal from "@/components/LoadingModal";
 import MonthYearBtn from "./MonthYearBtn";
 import PieChartComponent from "./PieChart";
 import { DateControllerWrapper } from "./style";
 
+const LineGraph = React.lazy(() => import("./LineGraph"));
+
 function Graph() {
   const monthButton = true;
   const yearButton = false;
+  const ClickedMoth = true;
   const toDay = new Date();
   const [date, setDate] = useState(toDay);
+  const [clickButtonColor, setClickButtonColor] = useState(true);
+  const [checkMonth, setCheckMonth] = useState(false);
+  const [graphTitle, setGraphTitle] = useState("월간");
+  const [commitData, setCommitData] = useState([]);
+
+  const getCommitsPerMonth = async () => {
+    const year = date.toISOString().slice(0, 4);
+    const data = await api.getCommitsTotalPerMonth(year);
+
+    if (data.success) {
+      const commitPerYear = await data.commitPerYear;
+
+      const createData = commitPerYear.map((commitCnt, index) => ({
+        name: `${date.toISOString().slice(0, 2)}.${index + 1}`,
+        commit: commitCnt,
+      }));
+      setCommitData(createData);
+    } else {
+      setCommitData([]);
+    }
+  };
+
+  useEffect(() => {
+    getCommitsPerMonth();
+  }, [date]);
 
   const changeDate = (value) => {
     const newDate = new Date(date.getFullYear() + value, date.getMonth());
@@ -31,46 +60,45 @@ function Graph() {
     setDate(toDay);
   };
 
-  const [clickButtonColor, setClickButtonColor] = useState(true);
-  const [checkMonth, setCheckMonth] = useState(false);
-  const [graphTitle, setGraphTitle] = useState("월간");
-
   const handleMonthBtn = useCallback(() => {
     if (monthButton) {
       setClickButtonColor(monthButton);
       setCheckMonth(false);
-      setGraphTitle(checkMonth ? "월간" : "년간");
+      setGraphTitle(ClickedMoth ? "월간" : "년간");
     }
   }, [graphTitle]);
+
   const handlYearBtn = useCallback(() => {
     if (!yearButton) {
-      setClickButtonColor(!monthButton);
+      setClickButtonColor(yearButton);
       setCheckMonth(true);
-      setGraphTitle(checkMonth ? "월간" : "년간");
+      setGraphTitle(!ClickedMoth ? "월간" : "년간");
     }
   }, [graphTitle]);
 
   return (
     <Container>
-      <DateControllerWrapper>
-        {!checkMonth && (
-          <DateController
-            date={date}
-            clickLeft={clickLeft}
-            clickRight={clickRight}
-            goToday={goToday}
-            month={false}
-          />
-        )}
-      </DateControllerWrapper>
+      <Suspense fallback={<LoadingModal />}>
+        <DateControllerWrapper>
+          {!checkMonth && (
+            <DateController
+              date={date}
+              clickLeft={clickLeft}
+              clickRight={clickRight}
+              goToday={goToday}
+              month={false}
+            />
+          )}
+        </DateControllerWrapper>
 
-      <MonthYearBtn
-        isClick={clickButtonColor}
-        handlYearBtn={handlYearBtn}
-        handleMonthBtn={handleMonthBtn}
-      />
-      <LineGraph graphTitle={graphTitle} />
-      <PieChartComponent />
+        <MonthYearBtn
+          isClick={clickButtonColor}
+          handlYearBtn={handlYearBtn}
+          handleMonthBtn={handleMonthBtn}
+        />
+        <LineGraph graphTitle={graphTitle} commitData={commitData} />
+        <PieChartComponent />
+      </Suspense>
     </Container>
   );
 }
