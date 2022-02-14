eb597e6f885ba4748beb9ae5236a0af0c4508026
YWTechIT <abcd8637@naver.com>
2022-02-15 00:02:09 +0900

    Add /api/users/goal POST goal GET goal test

---
+++ Backend/routes/api/users.test.js
@@ -0,0 +1,43 @@
+import "regenerator-runtime";
+import request from "supertest";
+import { GOAL } from "../../model/default/index.js";
+import app from "../../server.js";
+
+describe("/api/users", () => {
+  const token =
+    "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU0NTQzMDEzIiwiZW1haWwiOiJ5d3RlY2hpdEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6IllXVGVjaElUIiwiaWF0IjoxNjQ0ODQ5NTA0LCJleHAiOjE2NDU0NTQzMDR9.KiW72eE8_ZNos42UYOa73_KohnoaiiHEYLeMFxO9eYw";
+
+  describe("/api/users/goal", () => {
+    test("POST Goal", async () => {
+      const response = await request(app)
+        .post("/api/users/goal")
+        .set("Cookie", token)
+        .send({ goal: GOAL });
+
+      const expectedStatus = 201;
+      const expectedBody = {
+        success: true,
+        goal: GOAL,
+      };
+
+      expect(response.statusCode).toEqual(expectedStatus);
+      expect(response._body).toStrictEqual(expectedBody);
+    });
+
+    test("GET Goal", async () => {
+      const response = await request(app)
+        .get("/api/users/goal")
+        .set("Cookie", token)
+        .send();
+
+      const expectedStatus = 200;
+      const expectedBody = {
+        success: true,
+        goal: GOAL,
+      };
+
+      expect(response.statusCode).toEqual(expectedStatus);
+      expect(response._body).toStrictEqual(expectedBody);
+    });
+  });
+});
