fc2c55dc77039f755ca2f00b40d5b19712bc507c
YWTechIT <abcd8637@naver.com>
2022-02-14 21:02:22 +0900

    Add target을 targetLength 자리수로 맞춰고 앞 자리에 원하는 값을 채워주는 test

--- Backend/utils/date.test.js
+++ Backend/utils/date.test.js
@@ -1,4 +1,4 @@
-import { checkLeapYear, getFebruary, year } from "./date.js";
+import { checkLeapYear, getFebruary, year, fillZero } from "./date.js";
 
 describe("윤년 test", () => {
   test("윤년입니다.", () => {
@@ -20,5 +20,14 @@ describe("윤년여부에 따른 February 배열 test", () => {
       ? Array.from({ length: 29 }, () => 0)
       : Array.from({ length: 28 }, () => 0);
     expect(february).toStrictEqual(answer);
+  });
+});
+
+describe("target을 targetLength 자리수로 맞춰고 앞 자리에 원하는 값을 채워주는 test", () => {
+  test(`2를 02로 변경`, () => {
+    const target = 2;
+    const targetLength = 2;
+    const padString = "0";
+    expect(fillZero(target, targetLength, padString)).toBe("02");
   });
 });
