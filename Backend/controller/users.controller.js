9231d7725b40c5ffeeed66a53f993c952e1477b3
YWTechIT <abcd8637@naver.com>
2022-02-15 00:36:06 +0900

    Update postResolutionController res.json

--- Backend/controller/users.controller.js
+++ Backend/controller/users.controller.js
@@ -164,9 +164,15 @@ export const getResolutionController = async (req, res) => {
 export const postResolutionController = async (req, res) => {
   try {
     await setResolution(req);
-    res.status(201);
+    res.status(201).json({
+      success: true,
+      resolution: req.body.resolution,
+    });
   } catch (err) {
-    res.status(500);
+    res.status(400).json({
+      success: false,
+      message: "Bad Request",
+    });
   }
 };
 
