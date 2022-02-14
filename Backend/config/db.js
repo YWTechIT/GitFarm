7fb3f161ec7009f3b00f019e407eec64dd0b14bd
YWTechIT <abcd8637@naver.com>
2022-02-14 23:14:10 +0900

    Remove console.log when connect db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,6 @@ import keys from "./keys.js";
 export default async () => {
   try {
     await mongoose.connect(keys.mongoURI);
-    console.log("Successfully connect mongoDB");
   } catch (err) {
     console.error(err.message);
   }
