f82a35291ba18fe018d8330f6dbdef24567bd793
황원준 <dittoavec@gmail.com>
2022-02-17 20:16:27 +0000

    220218 4주차 - 프론트엔드 MR 요청

--- Frontend/src/pages/graph/style.js
+++ Frontend/src/pages/graph/style.js
@@ -1,6 +1,22 @@
 /* eslint-disable */
 import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 170px;
+  color: ${(props) => props.theme.darkGray};
+`;
+
 export const DateControllerWrapper = styled.div`
   height: 20px;
+`;
+
+export const ResponsiveDiv = styled.div`
+  @media ${({ theme }) => theme.device.laptop} {
+    display: flex;
+    align-items: center;
+    margin-top: 25px;
+  }
 `;
