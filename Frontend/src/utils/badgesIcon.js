a3ba8837f71566316ddc1065ccd3c49fe95f05ef
지영 서 <stseo012@gmail.com>
2022-02-18 09:06:03 +0000

    220218 1주차 - 프론트엔드 MR 요청

--- Frontend/src/utils/badgesIcon.js
+++ Frontend/src/utils/badgesIcon.js
@@ -4,14 +4,13 @@ import Days14 from "@/assets/icon/badge/14days.svg";
 import Days21 from "@/assets/icon/badge/21days.svg";
 import Days28 from "@/assets/icon/badge/28days.svg";
 
-import Achievement50 from "@/assets/icon/badge/50achievement.svg";
-import Achievement100 from "@/assets/icon/badge/100achievement.svg";
+import Achievement500 from "@/assets/icon/badge/500achievement.svg";
+import Achievement1000 from "@/assets/icon/badge/1000achievement.svg";
+import Achievement5000 from "@/assets/icon/badge/5000achievement.svg";
 
 import FarmStage1 from "@/assets/icon/badge/farm-stage1.svg";
 import FarmStage2 from "@/assets/icon/badge/farm-stage2.svg";
 import FarmStage3 from "@/assets/icon/badge/farm-stage3.svg";
-
-import FirstCommit from "@/assets/icon/badge/first-commit.svg";
 
 import Level1 from "@/assets/icon/level/level1.svg";
 import Level2 from "@/assets/icon/level/level2.svg";
@@ -28,14 +27,13 @@ export const days14 = <Days14 />;
 export const days21 = <Days21 />;
 export const days28 = <Days28 />;
 
-export const achievement50 = <Achievement50 />;
-export const achievement100 = <Achievement100 />;
+export const achievement500 = <Achievement500 />;
+export const achievement1000 = <Achievement1000 />;
+export const achievement5000 = <Achievement5000 />;
 
 export const farmStage1 = <FarmStage1 />;
 export const farmStage2 = <FarmStage2 />;
 export const farmStage3 = <FarmStage3 />;
-
-export const firstCommit = <FirstCommit />;
 
 export const level1 = <Level1 />;
 export const level2 = <Level2 />;
