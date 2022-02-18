fe6c8852044bf1e32459f68dc55bff1e6f3014d5
황원준 <dittoavec@gmail.com>
2022-02-18 07:47:53 +0000

    220218 - 프론트엔드 MR 요청

--- Frontend/src/pages/myPage/AccountSettings/style.js
+++ Frontend/src/pages/myPage/AccountSettings/style.js
@@ -12,9 +12,6 @@ export const Wrapper = styled.div`
 `;
 
 export const Content = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   margin-top: 32px;
   cursor: pointer;
 
--- Frontend/src/pages/setting/index.js
+++ Frontend/src/pages/setting/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "@/components/Container/style";
 import NextArrow from "@/assets/icon/next-arrow.svg";
+import * as api from "@/api";
 import LogoutModal from "./LogoutModal";
 import DeleteAccountModal from "./DeleteAccountModal";
 import SettingItem from "../myPage/SettingItem";
@@ -9,6 +10,8 @@ import * as Settings from "./style";
 function Setting() {
   const [openLogoutModal, setOpenLogoutModal] = useState(false);
   const [openDeleteAccountModal, setOpenDeleteAccountModal] = useState(false);
+  const [myId, setMyId] = useState("");
+
   const ModalLogoutOpenHandler = () => {
     setOpenLogoutModal(true);
   };
@@ -16,12 +19,23 @@ function Setting() {
     setOpenDeleteAccountModal(true);
   };
 
+  const getRank = async () => {
+    const rankData = await api.getRank();
+    if (rankData.success) {
+      setMyId(rankData.data.myRank.username);
+    }
+  };
+
+  useEffect(() => {
+    getRank();
+  }, []);
+
   return (
     <Container>
       <Settings.Wrapper>
         <Settings.Title>GitHub 계정관리</Settings.Title>
         <Settings.ItemWrapper>
-          <SettingItem subTitle="로그인 계정" />
+          <SettingItem subTitle="로그인 계정" setCursor content={myId} />
           <SettingItem
             subTitle="계정 로그아웃"
             content={<NextArrow />}
