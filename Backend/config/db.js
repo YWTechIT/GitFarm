ddfa2765af0b7f5768f4d32c52f8b0140318841d
YWTechIT <abcd8637@naver.com>
2022-03-16 23:44:39 +0900

    Update batch: use updateMany from update

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -10,7 +10,7 @@ export default async () => {
     //batch jobs
     schedule.scheduleJob("* * 1 1,4,7,10 *", async () => {
       console.log("Reset totalScore in commit db");
-      await Commit.update({}, { $set: { totalScore: 0 } });
+      await Commit.updateMany({}, { $set: { totalScore: 0 } });
     });
   } catch (err) {
     console.error(err.message);
