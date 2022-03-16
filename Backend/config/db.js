fecbdbe389c07401fed0eeb1aeb5beea47191222
Jinwon Jeong <wlsdnjs16521@gmail.com>
2022-03-16 14:43:21 +0000

    Feature batch

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,17 @@
 /* eslint-disable import/extensions */
 import mongoose from "mongoose";
 import keys from "./keys.js";
+import schedule from "node-schedule";
+import { Commit } from "../model/index.js";
 
 export default async () => {
   try {
     await mongoose.connect(keys.mongoURI);
+    //batch jobs
+    schedule.scheduleJob("* * 1 1,4,7,10 *", async () => {
+      console.log("Reset totalScore in commit db");
+      await Commit.update({}, { $set: { totalScore: 0 } });
+    });
   } catch (err) {
     console.error(err.message);
   }
