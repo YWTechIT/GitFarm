b49f9b6c5cd46f29caab466f672c411c259ea45b
황원준 <dittoavec@gmail.com>
2022-03-23 04:01:21 +0000

    Feature custom hook test

--- Frontend/fixtures/graph.js
+++ Frontend/fixtures/graph.js
@@ -150,10 +150,28 @@ export const mockReposLanguage = [
 ];
 
 export const mockCommitData = [
-  { name: "1월", commit: 34 },
-  { name: "2월", commit: 18 },
-  { name: "3월", commit: 34 },
+  { commit: 34, name: "1월" },
+  { commit: 18, name: "2월" },
+  { commit: 0, name: "3월" },
 ];
+
+export const mockCommitsTotalPerMonthData = {
+  success: true,
+  id: 2022,
+  commitEachMonth: [0, 34, 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
+
+export const mockFalseCommitsTotalPerMonthData = {
+  success: false,
+  id: 2022,
+  commitEachMonth: [0, 34, 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
+
+export const mockWrongCommitsTotalPerMonthData = {
+  success: true,
+  id: 2022,
+  commitEachMonth: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
 
 export const languageRatioArray = [
   { name: "JavaScript", value: 43.24 },
@@ -161,3 +179,89 @@ export const languageRatioArray = [
   { name: "HTML", value: 21.62 },
   { name: "CSS", value: 2.7 },
 ];
+
+export const mockReposLanguageData = {
+  success: true,
+  languages: [
+    { repo: "have-u-tried-this", language: "TypeScript" },
+    { repo: "bucketplace-assigngment", language: "TypeScript" },
+    { repo: "alice-sw-engineer-track", language: "HTML" },
+    { repo: "ci-cd-practice", language: "JavaScript" },
+    { repo: "commento-assignment", language: "TypeScript" },
+    { repo: "consider-of-reusable-input-component", language: "TypeScript" },
+    { repo: "custombucks-api", language: "JavaScript" },
+    { repo: "get-address-with-EXIF", language: "JavaScript" },
+    { repo: "have-u-tried-this", language: "TypeScript" },
+    { repo: "image-drag-and-drop", language: "JavaScript" },
+    { repo: "image-upload-test", language: "JavaScript" },
+    { repo: "infinite-scroll", language: "HTML" },
+    { repo: "intersection-observer-pratice", language: "TypeScript" },
+    { repo: "JP2-this-week-I-learned", language: "CSS" },
+    { repo: "next-js-init-practice", language: "JavaScript" },
+    { repo: "now_news", language: "HTML" },
+    { repo: "project-military-test", language: "JavaScript" },
+    { repo: "quiz_project", language: "JavaScript" },
+    { repo: "quiz_project_practice", language: "JavaScript" },
+    { repo: "react-webpack-babel-jest-bolierplate", language: "JavaScript" },
+    { repo: "redux_example", language: "JavaScript" },
+    { repo: "ssr-with-react-node-practice", language: "JavaScript" },
+    { repo: "starbucks-recipe-project", language: "TypeScript" },
+    { repo: "starter-quiz-app-week07-00", language: "JavaScript" },
+    { repo: "test_shop", language: "JavaScript" },
+    { repo: "text-ellipsis-example", language: "HTML" },
+    { repo: "todo-list-project", language: "TypeScript" },
+    { repo: "todo-list-with-localstorage", language: "HTML" },
+    { repo: "todo-list-with-mvc-patterns", language: "TypeScript" },
+    { repo: "todo-list_with_typescript", language: "TypeScript" },
+    { repo: "typescript_tips", language: "TypeScript" },
+    { repo: "use-book-search-hook", language: "HTML" },
+    { repo: "usePosts-example", language: "TypeScript" },
+    { repo: "velopertStudy", language: "JavaScript" },
+    { repo: "ywBot", language: "JavaScript" },
+    { repo: "YWTech1", language: "HTML" },
+    { repo: "YWTechIT.github.io", language: "HTML" },
+  ],
+};
+
+export const mockFalseReposLanguageData = {
+  success: false,
+  languages: [
+    { repo: "have-u-tried-this", language: "TypeScript" },
+    { repo: "bucketplace-assigngment", language: "TypeScript" },
+    { repo: "alice-sw-engineer-track", language: "HTML" },
+    { repo: "ci-cd-practice", language: "JavaScript" },
+    { repo: "commento-assignment", language: "TypeScript" },
+    { repo: "consider-of-reusable-input-component", language: "TypeScript" },
+    { repo: "custombucks-api", language: "JavaScript" },
+    { repo: "get-address-with-EXIF", language: "JavaScript" },
+    { repo: "have-u-tried-this", language: "TypeScript" },
+    { repo: "image-drag-and-drop", language: "JavaScript" },
+    { repo: "image-upload-test", language: "JavaScript" },
+    { repo: "infinite-scroll", language: "HTML" },
+    { repo: "intersection-observer-pratice", language: "TypeScript" },
+    { repo: "JP2-this-week-I-learned", language: "CSS" },
+    { repo: "next-js-init-practice", language: "JavaScript" },
+    { repo: "now_news", language: "HTML" },
+    { repo: "project-military-test", language: "JavaScript" },
+    { repo: "quiz_project", language: "JavaScript" },
+    { repo: "quiz_project_practice", language: "JavaScript" },
+    { repo: "react-webpack-babel-jest-bolierplate", language: "JavaScript" },
+    { repo: "redux_example", language: "JavaScript" },
+    { repo: "ssr-with-react-node-practice", language: "JavaScript" },
+    { repo: "starbucks-recipe-project", language: "TypeScript" },
+    { repo: "starter-quiz-app-week07-00", language: "JavaScript" },
+    { repo: "test_shop", language: "JavaScript" },
+    { repo: "text-ellipsis-example", language: "HTML" },
+    { repo: "todo-list-project", language: "TypeScript" },
+    { repo: "todo-list-with-localstorage", language: "HTML" },
+    { repo: "todo-list-with-mvc-patterns", language: "TypeScript" },
+    { repo: "todo-list_with_typescript", language: "TypeScript" },
+    { repo: "typescript_tips", language: "TypeScript" },
+    { repo: "use-book-search-hook", language: "HTML" },
+    { repo: "usePosts-example", language: "TypeScript" },
+    { repo: "velopertStudy", language: "JavaScript" },
+    { repo: "ywBot", language: "JavaScript" },
+    { repo: "YWTech1", language: "HTML" },
+    { repo: "YWTechIT.github.io", language: "HTML" },
+  ],
+};
